a2dc331221519cf8ad4e8068f912d54095595c35
Ian Brayoni <brayoni.ian@gmail.com>
2017-03-01 02:22:49 +0300

    logic flows for both csv and log formatters

--- inheritance/inheritancely.py
+++ inheritance/inheritancely.py
@@ -1,8 +1,11 @@
+#! /usr/bin/env python3
+
 """
 Challenge: Implement a library exposing two interfaces, CSVFormatter and
            LogFormatter, borrowing heavily from OOP(Inheritance) to achieve a
            brief and maintainable solution.
 """
+
 import abc
 import datetime
 
@@ -10,36 +13,44 @@ import datetime
 class WriteFile(object):
     __metaclass__ = abc.ABCMeta
 
-    def __init__(self, writer, filename):
-        self.writer = writer
+    def __init__(self, filename):
         self.filename = filename
-        self.value = 0
 
-    def set_val(self, value):
-        self.value = value
-
-    def get_val(self):
-        return self.value
+    def write_line(self, text):
+        with open(self.filename, 'a') as f_obj:
+            f_obj.write(text + '\n')
 
     @abc.abstractmethod
     def write(self):
-        with open(self.filename, 'w'):
-            self.writer.write(self.value)
         return
 
 
 class CSVFormatter(WriteFile):
 
-    def write(self):
-        pass
+    def __init__(self, filename, delimiter):
+        super(CSVFormatter, self).__init__(filename)
+        self.delimiter = delimiter
+
+    def write(self, input_list):
+        text = self.delimiter.join(map(str, input_list))
+        self.write_line(text)
 
 
 class LogFormatter(WriteFile):
 
     @staticmethod
-    def date_formatter():
+    def datetime_formatter():
         return datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
 
-    def write(self):
-        self.value = self.date_formatter() + '  ' + self.value + '\n'
-        return super(LogFormatter, self).write(self.value)
+    def write(self, input_line):
+        self.write_line('{datetime}  {line}'.format(
+            datetime=self.datetime_formatter(),
+            line=input_line
+        ))
+
+if __name__ == "__main__":
+    write_csv = CSVFormatter('text.csv', ',')
+    write_log = LogFormatter('text.log')
+
+    write_log.write("test log entry")
+    write_csv.write([1, 2, 3, 4, 5])
