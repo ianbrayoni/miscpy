7a21f01658c0564d31ac300268d84f1c79833aad
Ian Brayoni <brayoni.ian@gmail.com>
2017-02-28 23:06:22 +0300

    Fleshed out abstractmethod write

--- inheritance/inheritancely.py
+++ inheritance/inheritancely.py
@@ -4,24 +4,40 @@ Challenge: Implement a library exposing two interfaces, CSVFormatter and
            brief and maintainable solution.
 """
 import abc
+import datetime
 
 
 class WriteFile(object):
     __metaclass__ = abc.ABCMeta
 
-    def __init__(self, writer):
+    def __init__(self, writer, filename):
         self.writer = writer
+        self.filename = filename
 
     @abc.abstractmethod
     def write(self):
+        with open(self.filename, 'w'):
+            self.writer.write()
         return
 
 
 class CSVFormatter(WriteFile):
+
+    def __init__(self):
+        pass
+
     def write(self):
         pass
 
 
 class LogFormatter(WriteFile):
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def date_formatter():
+        return datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
+
     def write(self):
         pass
