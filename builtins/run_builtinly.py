8e4eef2b3fee5de6bf43857e6f8907078cdc601e
Ian Brayoni <brayoni.ian@gmail.com>
2017-03-04 16:50:48 +0300

    Add util script

---
+++ builtins/run_builtinly.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+
+"""
+    Usages:
+    ./run_builtinly.py                          (reads out the entire config
+                                                dict)
+    ./run_builtinly.py  this_key this_value    (sets this_key and this_value
+                                                in the dict)
+"""
+
+import sys
+from .builtinly import ConfigDict
+
+app_conf = ConfigDict('app.cfg')
+
+if len(sys.argv) == 3:
+    key = sys.argv[1]
+    value = sys.argv[2]
+    print("Writing data: {0} {1}".format(key, value))
+    app_conf[key] = value
+
+else:
+    print("Reading data:")
+    for key in app_conf.keys():
+        print("   {0} = {1}".format(key, app_conf[key]))
