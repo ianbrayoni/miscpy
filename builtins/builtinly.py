f185e738afa71f6ddebb0bd4e6d6745d30513e74
Ian Brayoni <brayoni.ian@gmail.com>
2017-03-04 17:08:30 +0300

    Updated docstring

--- builtins/builtinly.py
+++ builtins/builtinly.py
@@ -1,7 +1,33 @@
 #! /usr/bin/env python3
 
 """
-Write to a config file by overriding the `dict` builtin
+Leverage the convenience of a dictionary to power a configuration file,
+which is simply a file of key-value pairs.
+
+A configuration file is used quite often in programming shops to hold
+values that don't belong in the Python script itself.
+Values like SQL queries, email addresses, and other configurable values
+should be stored outside of the script, because they may change when the
+code itself doesn't need to be changed.
+
+The structre of a config file could take many forms, and one of them is
+a simple key=value syntax, with one key/value pair per line.
+This is simple and straightforward, so we'll use it.
+
+What's great about using a built-in structure like a dictionary as
+the interface to a configuration file is that any Python programmer will
+immediately know how to use it.  The instructions are so simple you almost
+don't need documentation:
+    "create a new ConfigDict object, then read and write keys and
+    values as desired" -- that's it.
+
+
+Our config file should looks like this:
+
+    sql_query=SELECT this FROM that WHERE condition
+    email_to=me@mydomain.com
+    num_retries=5
+
 """
 import os
 
